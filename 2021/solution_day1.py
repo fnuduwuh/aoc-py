482127382e2d9e0f91fdd5775275a26c7725ac81
fdw <fddewaard@gmail.com>
2023-12-02 23:28:36 +0100

    update

--- 2021/solution_day1.py
+++ 2021/solution_day1.py
@@ -7,13 +7,13 @@ def get_data():
 def run_calc(part):
     increases = 0
     data = get_data()
-    for index, line in enumerate(data):
+    for i in range(len(data)):
         if part == 1:
-            if int(line) > int(data[index - 1]): increases += 1
+            if int(data[i]) > int(data[i - 1]): increases += 1
         else:
-            if index + 3 < len(data):
-                window_one = int(line) + int(data[index + 1]) + int(data[index + 2])
-                window_two = int(data[index + 1]) + int(data[index + 2]) + int(data[index + 3])
+            if i + 3 < len(data):
+                window_one = int(data[i]) + int(data[i + 1]) + int(data[i + 2])
+                window_two = window_one - int(data[i]) + int(data[i + 3])
                 if window_one < window_two: increases += 1
     print('Solution to part', part, ':', increases)
 
