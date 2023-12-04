09d30815d3d77a9a40770395c896ab77ffabc0f3
fdw <fddewaard@gmail.com>
2023-12-04 16:01:46 +0100

    nog beetje refactor

--- 2023/day4/solution.py
+++ 2023/day4/solution.py
@@ -5,27 +5,8 @@ def get_data():
     return open('input.txt', 'r').read().split('\n')
 
 
-def part_one():
-    data = get_data()
+def calc_cards():
     score_total = 0
-    for line in data:
-        if line == '':
-            print('the result is:', score_total)
-            break
-        score_line = 0
-        line = line.split(':')[1]
-        winning_no = line.split('|')[0].split()
-        actual_no = line.split('|')[1].split()
-        for num in actual_no:
-            if num in winning_no:
-                if score_line == 0:
-                    score_line = 1
-                else:
-                    score_line *= 2
-        score_total += score_line
-
-
-def part_two():
     data = get_data()
     card_count = {}
     for i in range(len(data) - 1):
@@ -34,13 +15,18 @@ def part_two():
         if line == '':
             break
         card_no = i + 1
+        score_line = 0
         line = line.split(':')[1]
         winning_no, actual_no = line.split('|')
         for j in range(1, len(list(set(winning_no.split()).intersection(actual_no.split()))) + 1):
-            if card_no < len(data):
-                card_count[card_no + j] += card_count.get(card_no)
+            card_count[card_no + j] += card_count.get(card_no)
+            if score_line == 0:
+                score_line = 1
+            else:
+                score_line *= 2
+        score_total += score_line
+    print('Result for part one is:', score_total)
     print('Result for part two is:', functools.reduce(lambda a, b: a + b, card_count.values()))
 
 
-part_one()
-part_two()
+calc_cards()
