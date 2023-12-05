bb3e16bbfe49a30d417bbdf3b638ca7313f8d6fe
fdw <fddewaard@gmail.com>
2023-12-05 16:52:23 +0100

    waardeloze oplossing

--- 2023/day5/solution.py
+++ 2023/day5/solution.py
@@ -2,10 +2,25 @@ def get_maps():
     return open('input.txt', 'r').read().split('\n\n')
 
 
+def get_match_reverse(start, conv_map):
+    lines = conv_map.split('\n')
+    del lines[0]
+    start = int(start)
+    for line in lines:
+        if line == '': break
+        lowest = int(line.split()[0])
+        plus_range = int(line.split()[2])
+        if lowest <= start < lowest + plus_range:
+            output = int(line.split()[1]) + (start - lowest)
+            return output
+    return start
+
+
 def get_match(start, conv_map):
     lines = conv_map.split('\n')
     del lines[0]
     start = int(start)
+
     for line in lines:
         if line == '': break
         lowest = int(line.split()[1])
@@ -17,13 +32,45 @@ def get_match(start, conv_map):
     return start
 
 
+def get_seed_val(val):
+    maps = get_maps()
+    del maps[0]
+    maps.reverse()
+    for conv_map in maps:
+        val = get_match_reverse(val, conv_map)
+    return val
+
+
+def part_two(): # slechte implementatie, brute force, duurt uitzonderlijk lang met mijn input
+    maps = get_maps()
+    seeds = maps[0].split()
+    del seeds[0]
+    location = 10000000
+    del maps[0]
+    seed_results = []
+    for index, seed in enumerate(seeds):
+        if index % 2 == 0:
+            seed_base = int(seeds[index])
+            seed_max = seed_base + int(seeds[index + 1])
+            seed_results.append((seed_base, seed_max))
+    maps.reverse()
+
+    while location >= 0:
+        result = get_seed_val(location)
+
+        for seed_result in seed_results:
+            if seed_result[0] <= result <= seed_result[1]:
+                print('bingo:', result, 'loc', location)
+                return location
+        location += 1
+
+
 def part_one():
     maps = get_maps()
     seeds = maps[0].split()
     del seeds[0]
     locations = []
     del maps[0]
-    print(maps)
 
     for seed in seeds:
         for conv_map in maps:
@@ -33,14 +80,5 @@ def part_one():
     print(locations)
 
 
-# seed_to_soil = maps[1].split('\n')
-# soil_to_fert = maps[2].split()
-# fert_to_water = maps[3].split()
-# water_to_light = maps[4].split()
-# light_to_temp = maps[5].split()
-# temp_to_hum = maps[6].split()
-# hum_to_loc = maps[7].split()
-# print(start)
-# print(seed_to_soil)
-
 part_one()
+part_two()
