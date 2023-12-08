99fd0aba4c5ebf78b79834839cb72965a51d03b3
fdw <fddewaard@gmail.com>
2023-12-08 12:03:06 +0100

    part 2

--- 2023/day8/solution.py
+++ 2023/day8/solution.py
@@ -1,3 +1,7 @@
+import functools
+import math
+
+
 def get_data():
     file = open('input.txt').read()
     nums = file.split('\n\n')[0]
@@ -22,25 +26,55 @@ locs_map = parse_locs(locs)
 step = 0
 
 
-def navigate(nums, current_loc, step):
+def navigate_recursively(nums, current_loc, step, dest='ZZZ'):
     for i, num in enumerate(nums):
         dir = ['L', 'R'].index(num)
-        dest = current_loc['destinations'][dir]
-        current_loc = next(loc for loc in locs_map if loc['id'] == dest)
+        current_loc = navigate(dir, current_loc)
         step += 1
-        if current_loc['id'] == 'ZZZ':
-            print('hooray, we\'re here! the distance travelled is', step)
-            return
+        if current_loc['id'] == dest:
+            print('hooray, we\'re here! the distance travelled for part one is', step)
+            return step
+        elif dest != 'ZZZ' and current_loc['id'][2] == dest:
+            print('min steps for node calculated:', step)
+            return step
         elif i == len(nums) - 1:
             nums += get_data()[0]
-            navigate(nums, current_loc, step)
+            return navigate_recursively(nums, current_loc, step, dest)
+
+
+def get_start_locs():
+    output = []
+    for loc in locs_map:
+        if loc['id'][2] == 'A':
+            output.append(loc)
+    return output
+
+
+def dest_reached(list_locs):
+    for loc in list_locs:
+        if loc['id'][2] != 'Z':
+            return False
+    return True
+
+
+def navigate(dir, current_loc):
+    dest = current_loc['destinations'][dir]
+    return next(loc for loc in locs_map if loc['id'] == dest)
 
 
 def part_one():
-    print(locs_map)
     current_loc = next(loc for loc in locs_map if loc['id'] == 'AAA')
+    navigate_recursively(nums, current_loc, 0)
 
-    navigate(nums, current_loc, 0)
 
+def part_two():
+    start_locs = get_start_locs()
+    no_of_steps = []
+    for loc in start_locs:
+        step_to_append = navigate_recursively(nums, loc, 0, 'Z')
+        no_of_steps.append(step_to_append)
+    print('hooray! we\'re here. distance travelled for part two is',
+          functools.reduce(lambda a, b: a * b // math.gcd(a, b), no_of_steps))
 
 part_one()
+part_two()
